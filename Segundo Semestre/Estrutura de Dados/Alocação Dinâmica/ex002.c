dabd651bff3031cc8b8704ab961292c2bd8c54a8
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-08-11 20:21:52 -0300

    2-6

--- Segundo Semestre/Estrutura de Dados/Alocação Dinâmica/ex002.c
+++ Segundo Semestre/Estrutura de Dados/Alocação Dinâmica/ex002.c
@@ -5,13 +5,42 @@
 #include<stdlib.h>
 #include<string.h>
 
-int palavra(){
+//int palavra(){
 
-}
+//}
 
 int main(){
-    int tamanhoString, i;
-    char entrada;
+    int tamanhoString, i, j;
+    char *entrada,
+    vogais[6] = {"aeiou"};
     
+    printf("Digite o um tamanho para a string: ");
+    scanf("%i", &tamanhoString);
+    entrada = (char *)malloc((tamanhoString + 1)*sizeof(char));
 
+    printf("Digite sua frase: ");
+    fflush(stdin);
+    fgets(entrada, tamanhoString + 1, stdin);
+    if(entrada == NULL){
+        printf("Erro ao receber entrada, tente novamente");
+        return 1;
+        
+    }
+    
+    fflush(stdin);
+
+    for(i = 0; i < strlen(entrada); i++){
+        for (j = 0; j < strlen(vogais); j++){
+            if(entrada[i] == vogais[j]){
+            entrada[i] = ' ';
+            break;
+            }
+
+        }
+
+    }
+    printf("Frase sem vogais: %s", entrada);
+
+    free(entrada);
+    return 0;
 }
---
+++ Segundo Semestre/Estrutura de Dados/Alocação Dinâmica/ex003.c
@@ -0,0 +1,34 @@
+//Faça um programa que leia um número N e:
+//Crie dinamicamente e leia um vetor de inteiro de N posições;
+//Leia um número inteiro X e conte e mostre os múltiplos desse número que existem no vetor.
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    int n, *vet, *multiplos, x, cont = 0, i = 0, j = 0;
+
+    printf("Digite um numero de posicoes pro vetor: ");
+    scanf("%i", n);
+
+    vet = malloc(n * sizeof(int));
+    multiplos = malloc(n * sizeof(int));
+
+    for(i = 0; i < n; i++){
+        printf("Numero %i: ", i + 1);
+        scanf("%i", &vet[i]);
+
+    }
+     printf("Digite um numero para saber se ele tem multiplos no vetor: ");
+     scanf("%i", &x);
+
+     for(i = 0; i < n; i++){
+        if(vet[i] % x = 0){
+            multiplos[j] = vet[i];
+            cont++;
+
+        }
+
+     }
+
+}
